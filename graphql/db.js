707fc45497fbe7a6a298a70cfe7ab12c3901dbf2
yang <yaaaaang57@naver.com>
2020-07-03 18:50:33 +0900

    end of course

--- graphql/db.js
+++ graphql/db.js
@@ -1,55 +1,35 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Star Wars",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Ring",
-        score: 86
-    },
-    {
-        id: 2,
-        name: "Lion King",
-        score: 39
-    },
-    {
-        id: 3,
-        name: "Avengers",
-        score: 99
-    },
-    {
-        id: 4,
-        name: "The Godfather",
-        score: 2
-    }
-]
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
+export const getMovies = (limit, rating) => {
+    return fetch(`${API_URL}?limit=${limit}&minimum_rating=${rating}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 };
 
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    }
-    else {
-        return false;
-    }
-};
+// export const getById = id => {
+//     const filteredMovies = movies.filter(movie => movie.id === id);
+//     return filteredMovies[0];
+// };
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length + 1,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie
-};
+// export const deleteMovie = id => {
+//     const cleanedMovies = movies.filter(movie => movie.id !== id);
+//     if (movies.length > cleanedMovies.length) {
+//         movies = cleanedMovies;
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// };
+
+// export const addMovie = (name, score) => {
+//     const newMovie = {
+//         id: movies.length + 1,
+//         name,
+//         score
+//     };
+//     movies.push(newMovie);
+//     return newMovie
+// };
