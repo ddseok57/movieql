85bce8c26a237892608df0229e1e46cb6747e6aa
yang <yaaaaang57@naver.com>
2020-08-17 01:09:47 +0900

    add something

--- graphql/db.js
+++ graphql/db.js
@@ -1,17 +1,26 @@
 import fetch from "node-fetch";
 
 const API_URL = "https://yts.mx/api/v2/list_movies.json";
+const DETAIL_URL = "https://yts.mx/api/v2/movie_details.json";
+const SUGGESTION_URL = "https://yts.mx/api/v2/movie_suggestions.json";
 
 export const getMovies = (limit, rating) => {
-    return fetch(`${API_URL}?limit=${limit}&minimum_rating=${rating}`)
+  return fetch(`${API_URL}?limit=${limit}&minimum_rating=${rating}`)
     .then(res => res.json())
     .then(json => json.data.movies);
 };
 
-// export const getById = id => {
-//     const filteredMovies = movies.filter(movie => movie.id === id);
-//     return filteredMovies[0];
-// };
+export const getById = id => {
+  return fetch(`${DETAIL_URL}?movie_id=${id}`)
+    .then(res => res.json())
+    .then(json => json.data.movie);
+};
+
+export const suggestById = id => {
+  return fetch(`${SUGGESTION_URL}?movie_id=${id}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
 
 // export const deleteMovie = id => {
 //     const cleanedMovies = movies.filter(movie => movie.id !== id);
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,15 @@
-import { getMovies } from "./db"
+import { getMovies, getById, suggestById } from "./db";
 
 const resolvers = {
-    Query: {
-        movies: (_, { limit, rating }) => getMovies(limit, rating),
-        // movie: (_, { id }) => getById(id),
-    },
-    // Mutation: {
-    //     addMovie: (_, { name, score }) => addMovie(name, score),
-    //     deleteMovie: (_, { id }) => deleteMovie(id)
-    // }
-}
+  Query: {
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getById(id),
+    suggestion: (_, { id }) => suggestById(id)
+  }
+  // Mutation: {
+  //     addMovie: (_, { name, score }) => addMovie(name, score),
+  //     deleteMovie: (_, { id }) => deleteMovie(id)
+  // }
+};
 
 export default resolvers;
